4723fc8b4708eba4106af1dccd03527440190950
l65713435-wq <l65713435@gmail.com>
2026-01-08 13:37:00 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,66 +9,50 @@ const enableWorkers = os.cpus().length !== 1;
 module.exports = {
     //// HOSTING CONFIGURATION ////
 
-    // Renderで動かすために '0.0.0.0' に変更
     bindingAddress: '0.0.0.0',
-    // Renderのポート番号を自動で読み込むように変更
     port: process.env.PORT || 10000,
-    crossDomainPort: 8081,
-    publicDir: path.join(__dirname, '../public'), // set to null to disable
+    // 予備ポートをnullにして、Renderの勘違いを防ぐよ
+    crossDomainPort: null, 
+    publicDir: path.join(__dirname, '../public'),
 
-    // enable or disable multithreading
     enableWorkers,
     workers: os.cpus().length,
-
-    // ssl object is either null or { key: fs.readFileSync('path/to/key'), cert: fs.readFileSync('path/to/cert') }
     ssl: null,
 
-    // RenderのURLに自動で合わせるように設定を変更
     getServerInfo: (req) => {
         const host = req ? req.headers.host : 'localhost';
-        return { hostname: host, port: 443, crossDomainPort: 443, protocol: 'https:' };
+        // Renderは外部からHTTPSで繋がるので設定を固定
+        return { hostname: host, port: 443, crossDomainPort: null, protocol: 'https:' };
     },
 
-    // enforce a password for creating new sessions. set to null to disable
     password: '',
-
-    // disable or enable localStorage sync
     disableLocalStorageSync: false,
+    restrictSessionToIP: false, // 学校などIPが変わる環境のためにfalseがおすすめ
 
-    // restrict sessions to be only used per IP
-    restrictSessionToIP: true,
-
-    // caching options for js rewrites.
     jsCache: new RammerheadJSFileCache(path.join(__dirname, '../cache-js'), 5 * 1024 * 1024 * 1024, 50000, enableWorkers),
-
-    // whether to disable http2 support or not
     disableHttp2: false,
 
     //// REWRITE HEADER CONFIGURATION ////
-
     stripClientHeaders: [],
     rewriteServerHeaders: {},
 
     //// SESSION STORE CONFIG ////
-
     fileCacheSessionConfig: {
         saveDirectory: path.join(__dirname, '../sessions'),
-        cacheTimeout: 1000 * 60 * 20, // 20 minutes
-        cacheCheckInterval: 1000 * 60 * 10, // 10 minutes
+        cacheTimeout: 1000 * 60 * 20,
+        cacheCheckInterval: 1000 * 60 * 10,
         deleteUnused: true,
         staleCleanupOptions: {
-            staleTimeout: 1000 * 60 * 60 * 24 * 3, // 3 days
+            staleTimeout: 1000 * 60 * 60 * 24 * 3,
             maxToLive: null,
-            staleCheckInterval: 1000 * 60 * 60 * 6 // 6 hours
+            staleCheckInterval: 1000 * 60 * 60 * 6
         },
         deleteCorruptedSessions: true,
     },
 
     //// LOGGING CONFIGURATION ////
-
     logLevel: process.env.DEVELOPMENT ? 'debug' : 'info',
     generatePrefix: (level) => `[${new Date().toISOString()}] [${level.toUpperCase()}] `,
-
     getIP: (req) => req.headers['x-forwarded-for'] || req.socket.remoteAddress
 };
 
